79448f40ee10c2e6dafedaf99cf46cc1f850fb14
Vincent <z5207667@ad.unsw.edu.au>
2023-03-08 17:54:13 +1100

    deeper into db, querying and subcollections

--- test.py
+++ test.py
@@ -1,44 +1,100 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from firebase_admin import auth
 import json
+import datetime
+
+class User(object):
+    def __init__(self, uid, email, display_name, is_admin, is_banned):
+        self.uid = uid
+        self.email = email
+        self.display_name = display_name
+        self.is_admin = is_admin
+        self.is_banned = is_banned
+        
+        
+    def to_dict(self):
+        return {
+              'uid': self.uid, 
+              'email': self.email,
+              'display_name': self.display_name,
+              'is_admin': self.is_admin,
+              'is_banned': self.is_banned
+        }
+
+class Project(object):
+    def __init__(self, pid, creator, date_created):
+        self.pid = pid
+        self.creator = creator
+        self.date_created = date_created
+        
+        
+    def to_dict(self):
+        return {
+              'pid': self.pid, 
+              'creator': self.creator,
+              'date_created': self.date_created,
+        }
 
 
-#not sure if we need this or not
-firebase_config={"apiKey": "AIzaSyCODWXHzh67zRV-xq9ZFAZ4sVaqKwUq9cY",
-  "authDomain": "taskforge-9aea9.firebaseapp.com",
-  "databaseURL": "https://taskforge-9aea9-default-rtdb.firebaseio.com",
-  "projectId": "taskforge-9aea9",
-  "storageBucket": "taskforge-9aea9.appspot.com",
-  "messagingSenderId": "221747524877",
-  "appId": "1:221747524877:web:8785cfe8e0847bd257ec44",
-  "measurementId": "G-5N1VTMSWEC"
-}
-
+class Task(object):
+    def __init__(self, tid, creator, date_created):
+        self.tid = tid
+        self.creator = creator
+        self.date_created = date_created
+        
+        
+    def to_dict(self):
+        return {
+              'tid': self.tid, 
+              'creator': self.creator,
+              'date_created': self.date_created,
+        }
 
 # Use a service account.
 cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c54a827d6c.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-#writing to db
-doc_ref = db.collection('users').document('how')
-doc_ref.set({
-    'first': 'random',
-    'middle': 'Mathison',
-    'last': 'Turing',
-    'born': 1912
-})
+a = User(0, 'x1@gmail.com', 'tim', True, False)
+b = User(1, 'x2@gmail.com', 'john', False, False)
+c = User(2, 'x3@gmail.com', 'alex', False, False)
+d = User(3, 'x4@gmail.com', 'whitney', False, False)
+e = User(4, 'x5@gmail.com', 'jen', True, False)
 
 
-#reading from db
 users_ref = db.collection('users')
-docs = users_ref.stream()
+users_ref.document('0').set(a.to_dict())
+users_ref.document('2').set(b.to_dict())
+users_ref.document('3').set(c.to_dict())
+users_ref.document('4').set(d.to_dict())
+users_ref.document('5').set(e.to_dict())
 
+
+#QUERYING THE USERS COLLECTION
+users_ref = db.collection('users')
+query = users_ref.where('is_admin', '==', 'True')
+
+docs = db.collection('users').where('is_admin', '==', True).stream()
 for doc in docs:
     print(f'{doc.id} => {doc.to_dict()}')
     
-    
-auth.create_user(email="emaillol@gmail.com", password="gaygay")
-auth.create_user(email="emaillol@gmail.com", password="gaygay")
+
+docs = db.collection('users').where('uid', '>', 3).stream()
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
+
+
+#ADDING NEW COLLECTION, PROJECTS
+
+p0 = Project(0, 0, datetime.datetime.now())
+t0 = Task(0, 0, datetime.datetime.now())
+
+projects_ref = db.collection('projects')
+projects_ref.document('0').set(p0.to_dict())
+
+
+#ADDING TASKS AS A SUBCOLLECTION INSIDE OF A PROJECT DOCUMENT
+p0_ref = db.collection('projects').document('0')
+t0_ref = p0_ref.collection('tasks').document('0')
+t0_ref.set(t0.to_dict())
