445f72e5710fb38e5a8d83d3d2ec5dfed8a4163c
Dabin Haam <dabin.haam@gmail.com>
2023-03-08 21:33:49 +1100

    implementing create_project

---
+++ backend/src/proj_master.py
@@ -0,0 +1,32 @@
+'''
+Feature: Project Master
+Functionalities:
+ - revive_completed_project()
+ - remove_project_member()
+ - request_leave_project()
+ - invite_to_project()
+'''
+
+'''
+Revives a project where its status has been set to complete,
+but have be able to bring it back into progress
+pid = project id
+uid = user id
+'''
+def revive_completed_project(pid, uid):
+    '''
+    check whether supplied pid exists
+    check whether supplied uid exists
+
+    set project's status back into review
+    '''
+    pass
+
+def remove_project_membner():
+    pass
+
+def requst_leave_project():
+    pass
+
+def invite_to_project():
+    pass
--- backend/src/projects.py
+++ backend/src/projects.py
@@ -1,1 +1,43 @@
+'''
+Feature: Project Management
+Functionalities:
+ - create_project()
+ - tba
+'''
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def create_project(uid, name, description, status, due_date, team_strength, picture):
+
+    if status == None:
+        status = "Not Started"
+    if due_date == None:
+        due_date = None
+    if team_strength == None:
+        team_strength = None
+    if picture == None:
+        picture = "bleh.png"
+
+    pid = 0
+
+    data = {
+        "uid": uid,
+        "name": name,
+        "description": description,
+        "status": status,
+        "due_date": due_date,
+        "team_strength": team_strength,
+        "picture": picture,
+    }
+
+    proj_ref = db.collection("projects_test").document(str(pid)).set(data)
+
+    return proj_ref.id
+
+if __name__ == "__main__":
+    create_project(0, "Project1", "bleh", None, None, None, None)
