29a3c7839801eab3ff7553eff07d0a5dd88a302c
Dabin Haam <dabin.haam@gmail.com>
2023-03-08 20:23:35 +1100

    starting to write tests for creating project, and project master features

---
+++ backend/tests/proj_master_test.py
@@ -0,0 +1,24 @@
+'''
+Blackbox testing of Project Master Feature
+'''
+
+import pytest
+
+def test_revive_completed_project():
+    '''
+    project = create_project(args)  # create_project returns the project id (pid)
+
+    # set project's status to complete
+
+    revive_completed_project(pid, uid)
+    
+    '''
+
+def test_remove_project_membner():
+    pass
+
+def test_requst_leave_project():
+    pass
+
+def test_invite_to_project():
+    pass
--- backend/tests/projects_test.py
+++ backend/tests/projects_test.py
@@ -1,1 +1,44 @@
+'''
+Blackbox testing of Project Management Feature
 
+*** currently only implementing create_project()
+'''
+
+import pytest
+from src.projects import *
+from datetime import date
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def test_create_project1():
+
+    # test for project creation
+    uid = 0
+    name = "Project1"
+    description = "Creating Project1 for testing"
+    status = "Not Started"
+    due_date = None
+    team_strength = None
+    picture = None
+    result = create_project(uid, name, description, status, due_date, team_strength, picture)
+
+    assert result == 0
+
+def test_create_project2():
+
+    # test for project creation
+    uid = 0
+    name = "Project2"
+    description = "Creating Project2 for testing"
+    status = "Completed"
+    due_date = date.fromisoformat("2023-12-31")
+    team_strength = 5
+    picture = "test1.jpg"
+    result = create_project(uid, name, description, status, due_date, team_strength, picture)
+
+    assert result == 1
