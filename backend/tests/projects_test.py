36f34f63492ae617cfe67b6809935d030dfc21d8
Dabin Haam <dabin.haam@gmail.com>
2023-03-09 22:08:02 +1100

    contuing implementing for create_project

--- backend/tests/projects_test.py
+++ backend/tests/projects_test.py
@@ -5,23 +5,23 @@ Blackbox testing of Project Management Feature
 '''
 
 import pytest
-from src.projects import *
-from datetime import date
+from datetime import datetime, date
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from src.projects import *
 
-cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+# cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+# app = firebase_admin.initialize_app(cred)
+# db = firestore.client()
 
-def test_create_project1():
+def test_create_project_use_default_vals():
 
     # test for project creation
-    uid = 0
-    name = "Project1"
-    description = "Creating Project1 for testing"
-    status = "Not Started"
+    uid = 1
+    name = "Project0"
+    description = "Creating Project0 for testing"
+    status = None
     due_date = None
     team_strength = None
     picture = None
@@ -29,16 +29,85 @@ def test_create_project1():
 
     assert result == 0
 
-def test_create_project2():
+    # # reset database
+    # db.collection("projects_test").document("0").delete()
+
+def test_create_project_every_args():
 
     # test for project creation
-    uid = 0
-    name = "Project2"
-    description = "Creating Project2 for testing"
-    status = "Completed"
-    due_date = date.fromisoformat("2023-12-31")
+    uid = 1
+    name = "Project1"
+    description = "Creating Project1 for testing"
+    status = "In Progress"
+    due_date = "2023-12-31"
     team_strength = 5
     picture = "test1.jpg"
+
     result = create_project(uid, name, description, status, due_date, team_strength, picture)
 
-    assert result == 1
+    assert result == 0
+
+    # # reset database
+    # db.collection("projects_test").document("0").delete()
+
+# def test_create_multiple_projects():
+
+#     # test for project1 creation
+#     uid = 0
+#     name = "Project1"
+#     description = "Creating Project1 for testing"
+#     status = None
+#     due_date = None
+#     team_strength = None
+#     picture = None
+
+#     result = create_project(uid, name, description, status, due_date, team_strength, picture)
+
+#     assert result == 0
+
+#     # test for project2 creation
+#     uid = 0
+#     name = "Project1"
+#     description = "Creating Project1 for testing"
+#     status = None
+#     due_date = None
+#     team_strength = None
+#     picture = None
+
+#     result = create_project(uid, name, description, status, due_date, team_strength, picture)
+
+#     assert result == 1
+
+# def test_create_project_invalid_uid():
+
+#     # test for project creation with invalid input
+#     uid = -1
+#     name = "Project1"
+#     description = "description"
+#     status = "Not Started"
+#     due_date = None
+#     team_strength = None
+#     picture = None
+
+#     with pytest.raises(ValueError):
+#         create_project(uid, name, description, status, due_date, team_strength, picture)
+
+#     # reset database
+#     db.collection("projects_test").document("0").delete()
+
+# def test_create_project_invalid_proj_name():
+
+#     # test for project creation with invalid input
+#     uid = 0
+#     name = -1
+#     description = -1
+#     status = -1
+#     due_date = -1
+#     team_strength = -1
+#     picture = -1
+
+#     with pytest.raises(ValueError):
+#         create_project(uid, name, description, status, due_date, team_strength, picture)
+
+#     # reset database
+#     db.collection("projects_test").document("0").delete()
