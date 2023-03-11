b084964c803c55d60085a72b8cdbccd9c7d2a06b
unknown <Francis Nguyen>
2023-03-12 07:31:30 +1100

    added classes.py with user database class

---
+++ backend/src/classes.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+class User(object):
+    def __init__(self, uid, is_admin, is_banned, achievements, projects, tasks):
+        self.uid = uid
+        self.is_admin = is_admin
+        self.is_banned = is_banned
+        self.achievements = achievements
+        self.projects = projects
+        self.tasks = tasks
+        
+        
+    def to_dict(self):
+        return {
+            'uid': self.uid, 
+            'is_admin': self.is_admin,
+            'is_banned': self.is_banned,
+            "achievements": self.achievements,
+            "projects": self.projects,
+            "tasks": self.tasks
+        }
