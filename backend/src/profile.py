fb057891113a1ed55d028899c36d5d897b7afa66
unknown <Francis Nguyen>
2023-03-12 07:10:05 +1100

    Started implementation of profile

--- backend/src/profile.py
+++ backend/src/profile.py
@@ -1,1 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
 
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+def create_user():
