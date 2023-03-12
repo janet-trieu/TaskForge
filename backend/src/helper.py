4d0af4eb72bc533f4134662b175afc16a6969fe5
Vincent <z5207667@ad.unsw.edu.au>
2023-03-12 13:35:37 +1100

    removed redundant helper functions and updated admin tests

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -1,16 +1,3 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-
-
-def get_user_by_email(email, db):
-    users_ref = db.collection('users')
-    users = db.collection('users').where('email', '==', email).stream()
-    for user in users:
-        return (user.to_dict())
-        
-def get_user_by_uid(uid, db):
-    users_ref = db.collection('users')
-    users = db.collection('users').where('uid', '==', uid).stream()
-    for user in users:
-        return (user.to_dict())
--- backend/tests/admin_test.py
+++ backend/tests/admin_test.py
@@ -3,102 +3,98 @@ import firebase_admin
 from firebase_admin import auth
 from src.admin import *
 from src.helper import *
+from src.profile import *
+
+
+# Set up
+# Use a service account.
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-8157d1424f.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 #Assuming that 2 users already exist, the first one is root admin and 2nd one is just a normal user
 #each test should somewhat reset for the next test
 @pytest.mark.order1
 def test_give_admin():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(not user.get('is_admin'))
-    
-    give_admin(admin.get('uid'), user.get('uid'))
-    assert(user.is_admin)
+    assert(is_admin(0))
+    assert(not is_admin(1))
+    give_admin(0, 1)
 
 @pytest.mark.order2
 def test_give_admin_to_admin():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    assert(admin.get('is_admin'))
+    assert(is_admin(0))
+    assert(is_admin(1))
     
-    give_admin(admin.get('uid'), admin.get('uid'))
-    assert(admin.is_admin)
+    give_admin(0, 1)
+    assert(is_admin(1))
 
 @pytest.mark.order3
 def test_ban_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(not user.get('is_banned'))
+    assert(is_admin(0))
+    assert(not is_banned(1))
     
-    ban_user(admin.get('uid'), user.get('uid'))
-    assert(user.is_banned)
+    ban_user(0, 1)
+    assert(is_banned(1))
 
 @pytest.mark.order4
 def test_ban_banned_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
+    assert(is_admin(0))
+    assert(is_banned(1))
     
-    ban_user(admin.get('uid'), user.get('uid'))
-    assert(user.is_banned)
-    
-    ban_user(admin.get('uid'), user.get('uid'))
-    assert(user.is_banned)
+    ban_user(0, 1)
+    assert(is_banned(1))
 
 
 #user is still banned from last test
 @pytest.mark.order5
 def test_unban_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(user.get('is_banned'))
+    assert(is_admin(0))
+    assert(is_banned(1))
     
-    unban_user(admin.get('uid'), user.get('uid'))
-    assert(not user.is_banned)
+    unban_user(0, 1)
+    assert(not is_banned(1))
 
 @pytest.mark.order6
 def test_unban_notbanned_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(not user.get('is_banned'))
+    assert(is_admin(0))
+    assert(not is_banned(1))
     
-    unban_user(admin.get('uid'), user.get('uid'))
-    assert(not user.is_banned)
+    unban_user(0, 1)
+    assert(not is_banned(1))
+
 
 @pytest.mark.order7
 def test_remove_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(not user.get('is_removed'))
+    assert(is_admin(0))
+    assert(not is_removed(1))
     
-    remove_user(admin.get('uid'), user.get('uid'))
-    assert(user.is_removed)
+    remove_user(0, 1)
+    assert(is_removed(1))
 
 
 #user still removed
 @pytest.mark.order8
 def test_remove_removed_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(user.get('is_removed'))
+    assert(is_admin(0))
+    assert(is_removed(1))
     
-    remove_user(admin.get('uid'), user.get('uid'))
-    assert(user.is_removed)
-    
-    remove_user(admin.get('uid'), user.get('uid'))
-    assert(user.is_removed)
-   
+    remove_user(0, 1)
+    assert(is_removed(1))
+
+
+#user still removed from last test
 @pytest.mark.order9
 def test_readd_removed_user():
-    admin = get_user_by_email('compgpt3900@gmail.com')
-    user = get_user_by_uid(1)
-    assert(admin.get('is_admin'))
-    assert(user.get('is_removed'))
+    assert(is_admin(0))
+    assert(is_removed(1))
     
-    readd_user(admin.get('uid'), user.get('uid'))
-    assert(not user.is_removed)
+    readd_user(0, 1)
+    assert(not is_removed(1))
+
+@pytest.mark.order10
+def test_readd_normal_user():
+    assert(is_admin(0))
+    assert(not is_removed(1))
+    
+    readd_user(0, 1)
+    assert(not is_removed(1))
