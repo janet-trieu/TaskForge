17fa2ff5e330d2223a2268258a1a43a2c6ddfa2e
Vincent <z5207667@ad.unsw.edu.au>
2023-03-12 19:08:08 +1100

    debugged admin functionalities

--- backend/src/admin.py
+++ backend/src/admin.py
@@ -1,70 +1,79 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from profile import *
-from global_counters import *
-from helper import *
+from src.profile import is_banned, is_admin, is_removed, get_user_ref
+from src.global_counters import *
 
-def give_admin(uid_admin, uid_user):
-    tuid = get_curr_tid()
-    
-    if (uid_admin is not int or uid_user is not int): raise TypeError('uids are ints')
-    if (uid_admin > tuid or uid_user > tuid): raise ValueError('uid invalid')
+def give_admin(uid_admin, uid_user): 
+    if (not isinstance(uid_admin, str) or not isinstance(uid_user, str)): raise TypeError('uids are strings')
+    if (not get_user_ref(uid_admin) or not get_user_ref(uid_user)): raise ValueError('uid invalid')
     if (is_banned(uid_admin)): raise ValueError('You are banned')
     if (is_banned(uid_user)): raise ValueError('Cannot make a banned user an admin')
     if (not is_admin(uid_admin)): raise ValueError('You are not an admin')
     if (is_admin(uid_user)): raise ValueError('User is already an admin')
     
-    user_ref = db.collection("users").document(uid_user)
+    user_ref = db.collection('users').where("uid", "==", uid_user).stream()
+    for doc in user_ref:
+        doc_name = doc.id
+    
+    user_ref = db.collection("users").document(doc_name)
     user_ref.update({'is_admin': True})
         
     
 
-def ban_user(uid_admin, uid_user):
-    tuid = get_curr_tid()
-    
-    if (uid_admin is not int or uid_user is not int): raise TypeError('uids are ints')
-    if (uid_admin > tuid or uid_user > tuid): raise ValueError('uid invalid')
+def ban_user(uid_admin, uid_user):  
+    if (not isinstance(uid_admin, str) or not isinstance(uid_user, str)): raise TypeError('uids are strings')
+    if (not get_user_ref(uid_admin) or not get_user_ref(uid_user)): raise ValueError('uid invalid')
     if (is_banned(uid_admin)): raise ValueError('You are banned')
     if (not is_admin(uid_admin)): raise ValueError('You are not an admin')
     if (is_banned(uid_user)): raise ValueError('User is already banned')
     
-    user_ref = db.collection("users").document(uid_user)
+    user_ref = db.collection('users').where("uid", "==", uid_user).stream()
+    for doc in user_ref:
+        doc_name = doc.id
+    
+    user_ref = db.collection("users").document(doc_name)
     user_ref.update({'is_banned': True})
     
-def unban_user(uid_admin, uid_user):
-    tuid = get_curr_tid()
-    
-    if (uid_admin is not int or uid_user is not int): raise TypeError('uids are ints')
-    if (uid_admin > tuid or uid_user > tuid): raise ValueError('uid invalid')
+def unban_user(uid_admin, uid_user): 
+    if (not isinstance(uid_admin, str) or not isinstance(uid_user, str)): raise TypeError('uids are strings')
+    if (not get_user_ref(uid_admin) or not get_user_ref(uid_user)): raise ValueError('uid invalid')
     if (is_banned(uid_admin)): raise ValueError('You are banned')
     if (not is_admin(uid_admin)): raise ValueError('You are not an admin')
     if (not is_banned(uid_user)): raise ValueError('User is not banned')
     
-    user_ref = db.collection("users").document(uid_user)
+    user_ref = db.collection('users').where("uid", "==", uid_user).stream()
+    for doc in user_ref:
+        doc_name = doc.id
+    
+    user_ref = db.collection("users").document(doc_name)
     user_ref.update({'is_banned': False})
 
 
 def remove_user(uid_admin, uid_user):
-    tuid = get_curr_tid()
-    
-    if (uid_admin is not int or uid_user is not int): raise TypeError('uids are ints')
-    if (uid_admin > tuid or uid_user > tuid): raise ValueError('uid invalid')
+    if (not isinstance(uid_admin, str) or not isinstance(uid_user, str)): raise TypeError('uids are strings')
+    if (not get_user_ref(uid_admin) or not get_user_ref(uid_user)): raise ValueError('uid invalid')
     if (is_banned(uid_admin)): raise ValueError('You are banned')
     if (not is_admin(uid_admin)): raise ValueError('You are not an admin')
     if (is_removed(uid_user)): raise ValueError('User is already removed')
     
-    user_ref = db.collection("users").document(uid_user)
+    user_ref = db.collection('users').where("uid", "==", uid_user).stream()
+    for doc in user_ref:
+        doc_name = doc.id
+    
+    user_ref = db.collection("users").document(doc_name)
     user_ref.update({'is_removed': True})
 
 def readd_user(uid_admin, uid_user):
-    tuid = get_curr_tid()
-    
-    if (uid_admin is not int or uid_user is not int): raise TypeError('uids are ints')
-    if (uid_admin > tuid or uid_user > tuid): raise ValueError('uid invalid')
+    if (not isinstance(uid_admin, str) or not isinstance(uid_user, str)): raise TypeError('uids are strings')
+    if (not get_user_ref(uid_admin) or not get_user_ref(uid_user)): raise ValueError('uid invalid')
     if (is_banned(uid_admin)): raise ValueError('You are banned')
     if (not is_admin(uid_admin)): raise ValueError('You are not an admin')
     if (not is_removed(uid_user)): raise ValueError('User is not removed')
     
-    user_ref = db.collection("users").document(uid_user)
+    user_ref = db.collection('users').where("uid", "==", uid_user).stream()
+    for doc in user_ref:
+        doc_name = doc.id
+    
+    user_ref = db.collection("users").document(doc_name)
     user_ref.update({'is_removed': False})
