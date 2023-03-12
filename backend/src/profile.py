3375b4c1713309c468f94264468b8ffc8178f3a9
unknown <Francis Nguyen>
2023-03-12 11:29:33 +1100

    created create user email function including helper function

--- backend/src/profile.py
+++ backend/src/profile.py
@@ -1,15 +1,33 @@
+# Imports
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import auth
+from global_counters import *
+from classes import *
 
-cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+tu_doc = db.collection("counters").document("total_user")
+### ========= Functions ========= ###
+### ========= Create User ========= ###
+def create_user_email(email, password, display_name):
+    user = auth.create_user(
+        email = email,
+        password = password,
+        display_name = display_name
+    )
 
-cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+    create_user_firestore(user.uid)
+    update_tuid()
+    print('Sucessfully created new user: {0}'.format(user.uid))
 
+### ========= Helper Functions ========= ###
+### ========= Create User in Firestore Database ========= ###
 
-def create_user():
+def create_user_firestore(uid):
+    users_ref = db.collection("users")
+    value = get_curr_tuid()
+    user = User(value ,uid, False, False, [], [], [])
+    
+    users_ref.document(str(value)).set(user.to_dict())
+
+create_user_email("ilovehotstinkymenunderwear@gmail.com", "pasword123itseasyasdoremi", "John's stinky underwear")
