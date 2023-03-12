123c449d6e8da3ec00b19e88adefdec4e878d5fa
Janet Trieu <janet.trieu@outlook.com>
2023-03-13 06:15:24 +1100

    init creation

---
+++ backend/tests/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import os
+dir_path = os.getcwd()
+sys.path.insert(0, dir_path)
+
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+app = firebase_admin.initialize_app(cred)
