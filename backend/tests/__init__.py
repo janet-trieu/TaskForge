151aa8a5ca667256080b453c6e3faef73018011f
Vincent <z5207667@ad.unsw.edu.au>
2023-03-13 21:17:48 +1100

    readded in init lines, accidentally commented out

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -1,11 +1,11 @@
-#import sys
-#import os
-#dir_path = os.getcwd()
-#sys.path.insert(0, dir_path)
+import sys
+import os
+dir_path = os.getcwd()
+sys.path.insert(0, dir_path)
 
-#import firebase_admin
-#from firebase_admin import credentials, auth
+import firebase_admin
+from firebase_admin import credentials, auth
 
-#cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
-#app = firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
+app = firebase_admin.initialize_app(cred)
 
