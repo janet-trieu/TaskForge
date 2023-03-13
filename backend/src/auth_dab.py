658df508712f58f651c0c3f19cdaab784dc8981d
Dabin Haam <dabin.haam@gmail.com>
2023-03-13 20:23:14 +1100

    implementing user invoked reset_password, that returns a link to be sent to the email address specified

--- backend/src/auth_dab.py
+++ backend/src/auth_dab.py
@@ -1,3 +1,15 @@
 '''
 Temporary file for authentication user-invoked reset password
 '''
+from firebase_admin import auth
+
+def get_reset_password_link(uid):
+
+    email = auth.get_user(uid).email
+
+    link = auth.generate_password_reset_link(email, {})
+
+    if link == None:
+        return -1
+
+    return link
--- backend/tests/auth_test_dab.py
+++ backend/tests/auth_test_dab.py
@@ -1,10 +1,14 @@
 '''
 Temporary test file for authentication user-invoked reset password
 '''
+
+from src.auth_dab import get_reset_password_link
+
 def test_reset_password():
 
-    email = "dabin.haam@gmail.com"
-    link = auth.generate_password_reset_link(email, {})
+    user_id = "3TssNFyMXmOtTGarpRtAZ7VrOd72"
 
-    assert not link == None
+    res = get_reset_password_link(user_id)
+
+    assert not res == -1
 
