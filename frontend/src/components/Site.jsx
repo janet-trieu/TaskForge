ff0a836453a64c22bd6f57fcecef0522a11476d1
Joseph Saad <joseph.saad137@gmail.com>
2023-03-14 01:43:06 +1100

    removed test page

--- frontend/src/components/Site.jsx
+++ frontend/src/components/Site.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home.jsx'
-import Test from '../pages/Test.jsx';
 import Header from './Header.jsx';
 import Sidebar from './Sidebar.jsx';
 
@@ -13,7 +12,6 @@ const Site = ({ firebaseApp }) => {
         <Header />
         <Routes>
           <Route path='/' element={<Home firebaseApp={firebaseApp} />} />
-          <Route path='/test' element={<Test />} />
         </Routes>
       </div>
     </div>
