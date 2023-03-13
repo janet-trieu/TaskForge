2d70e8cf41a9e052a3cdb3d3ca00398dc0c73f51
Joseph Saad <joseph.saad137@gmail.com>
2023-03-14 01:28:51 +1100

    updated sidebar font size

--- frontend/src/components/Site.jsx
+++ frontend/src/components/Site.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home.jsx'
 import Test from '../pages/Test.jsx';
+import Header from './Header.jsx';
 import Sidebar from './Sidebar.jsx';
 
 const Site = ({ firebaseApp }) => {
@@ -8,6 +9,7 @@ const Site = ({ firebaseApp }) => {
     <div id='site-container'>
       <Sidebar firebaseApp={firebaseApp}/>
       <div className='vertical-line' />
+      <Header />
       <Routes>
         <Route path='/' element={<Home firebaseApp={firebaseApp} />} />
         <Route path='/test' element={<Test />} />
