0357189ff794ed4830acb3324e1dd58e391a9bd5
Vincent <z5207667@ad.unsw.edu.au>
2023-03-14 23:47:27 +1100

    changed document references to uuid instead of int ids

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,4 +1,4 @@
-import sys
+"""import sys
 import os
 dir_path = os.getcwd()
 sys.path.insert(0, dir_path)
@@ -7,4 +7,4 @@ import firebase_admin
 from firebase_admin import credentials
 
 cred = credentials.Certificate('taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json')
-app = firebase_admin.initialize_app(cred)
+app = firebase_admin.initialize_app(cred)"""
