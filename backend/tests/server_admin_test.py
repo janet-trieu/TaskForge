61d450e35181b681fbe7de28d13568ce80b1d9b9
Vincent <z5207667@ad.unsw.edu.au>
2023-03-15 01:34:37 +1100

    server tests

--- backend/tests/server_admin_test.py
+++ backend/tests/server_admin_test.py
@@ -4,134 +4,96 @@ from subprocess import Popen, PIPE
 import signal
 from time import sleep
 import requests
-import json
-import urllib
+ 
+url = 'http://127.0.0.1:5000'
 
-
-# Use this fixture to get the URL of the server. It starts the server for you,
-# so you don't need to.
-@pytest.fixture(name="url")
-def fixture_url():
-    url_re = re.compile(r' \* Running on ([^ ]*)')
-    server = Popen(["python3", "src/server.py"], stderr=PIPE, stdout=PIPE)
-    line = server.stderr.readline()
-    local_url = url_re.match(line.decode())
-    if local_url:
-        yield local_url.group(1)
-        # Terminate the server
-        server.send_signal(signal.SIGINT)
-        waited = 0
-        while server.poll() is None and waited < 5:
-            sleep(0.1)
-            waited += 0.1
-        if server.poll() is None:
-            server.kill()
-    else:
-        server.kill()
-        raise Exception("Couldn't get URL from local server")
-        
-
-def test_give_admin_success(url):
+def test_give_admin_success():
     """
     Successfully giving admin to another user
     """
-    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123',}
+    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123'}
     resp = requests.post(url + '/admin/give_admin', json=json_dict)
 
-    payload = resp.json()
-
-    assert payload == 0
     assert resp.status_code == 200
 
-def test_give_admin_failure(url):
+def test_give_admin_failure():
     """
     Giving an int instead of a string
     """
-    json_dict = {'uid_admin': 0, 'uid_user': 1,}
+    json_dict = {'uid_admin': 'hi', 'uid_user': 'aye'}
     resp = requests.post(url + '/admin/give_admin', json=json_dict)
 
     assert resp.status_code == 400
 
-def test_ban_user_success(url):
+def test_ban_user_success():
     """
     Successfully banning user by an admin
     """
-    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123',}
+    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123'}
     resp = requests.post(url + '/admin/ban_user', json=json_dict)
 
-    payload = resp.json()
-
-    assert payload == 0
     assert resp.status_code == 200
     
-def test_ban_user_failure(url):
+def test_ban_user_failure():
     """
     Giving an int instead of a string
     """
-    json_dict = {'uid_admin': 0, 'uid_user': 1,}
+    json_dict = {'uid_admin': 0, 'uid_user': 1}
     resp = requests.post(url + '/admin/ban_user', json=json_dict)
 
     assert resp.status_code == 400
     
-def test_unban_user_success(url):
+def test_unban_user_success():
     """
     Successfully unbanning user by an admin
     """
-    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123',}
+    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123'}
     resp = requests.post(url + '/admin/unban_user', json=json_dict)
 
-    payload = resp.json()
-
-    assert payload == 0
     assert resp.status_code == 200
     
-def test_unban_user_failure(url):
+def test_unban_user_failure():
     """
     Giving an int instead of a string
     """
-    json_dict = {'uid_admin': 0, 'uid_user': 1,}
+    json_dict = {'uid_admin': 0, 'uid_user': 1}
     resp = requests.post(url + '/admin/unban_user', json=json_dict)
 
     assert resp.status_code == 400
 
-def test_remove_user_success(url):
+def test_remove_user_success():
     """
     Successfully removing user by an admin
     """
-    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123',}
+    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123'}
     resp = requests.post(url + '/admin/remove_user', json=json_dict)
 
-    payload = resp.json()
-
-    assert payload == 0
     assert resp.status_code == 200
    
-def test_remove_user_failure(url):
+def test_remove_user_failure():
     """
     Giving an int instead of a string
     """
-    json_dict = {'uid_admin': 0, 'uid_user': 1,}
+    json_dict = {'uid_admin': 0, 'uid_user': 1}
     resp = requests.post(url + '/admin/remove_user', json=json_dict)
 
     assert resp.status_code == 400
    
-def test_readd_user_success(url):
+def test_readd_user_success():
     """
     Successfully readding user by an admin
     """
-    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123',}
-    resp = requests.post(url + '/admin/readding_user', json=json_dict)
+    json_dict = {'uid_admin': 'sklzNex5udNeOd65uvsuGAYBNkH2', 'uid_user': 'xyzabc123'}
+    resp = requests.post(url + '/admin/readd_user', json=json_dict)
 
-    payload = resp.json()
 
-    assert payload == 0
     assert resp.status_code == 200
     
-def test_readd_user_failure(url):
+def test_readd_user_failure():
     """
     Giving an int instead of a string
     """
-    json_dict = {'uid_admin': 0, 'uid_user': 1,}
+    json_dict = {'uid_admin': 0, 'uid_user': 1}
     resp = requests.post(url + '/admin/readd_user', json=json_dict)
 
     assert resp.status_code == 400
