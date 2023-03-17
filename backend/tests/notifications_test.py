1be8108f0123eb8a3be454b2493bb28e7d51e9f6
unknown <janet.trieu@outlook.com>
2023-03-18 01:19:08 +1100

    Final testing for project_invite

--- backend/tests/notifications_test.py
+++ backend/tests/notifications_test.py
@@ -7,22 +7,35 @@ from firebase_admin import auth
 
 from src.profile_page import *
 from src.notifications import *
+from src.proj_master import *
+from src.global_counters import *
+
+from tests.test_helpers import *
 
 # ============ SET UP ============ #
 db = firestore.client()
 
-create_user_email("notificationtest0@gmail.com", "password123", "John Doe")
-create_user_email("notificationtest1@gmail.com", "password123", "Jane Doe")
-""" pid_expected = 7357
-tid_expected = 1010
-rid_expected = 400 """
-""" db.collection('projects').document(str(pid_expected)).set({'name':'Project Notification !!! NOTIFICATION TEST'})
-db.collection('tasks').document(str(tid_expected)).set({'name':'Task Notification !!! NOTIFICATION TEST'})
-db.collection('achievements').document('night_owl').set({'name':'Night Owl !!! NOTIFICATION TEST'})
-db.collection('reviews').document(str(rid_expected)).set({'uid':'notifytestid1'}) """
+try:
+    create_user_email("notificationtest0@gmail.com", "password123", "John Doe")
+    create_user_email("notificationtest1@gmail.com", "password123", "Jane Doe")
+    create_user_email("notificationtest2@gmail.com", "password123", "Richard Roe")
+except auth.EmailAlreadyExistsError:
+    pass
 
 user_id0 = auth.get_user_by_email("notificationtest0@gmail.com").uid
 user_id1 = auth.get_user_by_email("notificationtest1@gmail.com").uid
+user_id2 = auth.get_user_by_email("notificationtest2@gmail.com").uid
+
+init_pid()
+
+pid_expected = create_project(user_id0, "Project N", "Description", None, None, None, None)
+
+"""
+tid_expected = 1010
+rid_expected = 400
+db.collection('tasks').document(str(tid_expected)).set({'name':'Task Notification !!! NOTIFICATION TEST'})
+db.collection('achievements').document('night_owl').set({'name':'Night Owl !!! NOTIFICATION TEST'})
+db.collection('reviews').document(str(rid_expected)).set({'uid':'notifytestid1'}) """
 
 def remove_test_data():
     '''
@@ -30,10 +43,10 @@ def remove_test_data():
     '''
     delete_user(user_id0)
     delete_user(user_id1)
-    clear_all_notifications(user_id0)
-    clear_all_notifications(user_id1)
-    """db.collection('projects').document(str(pid_expected)).delete()
-    db.collection('tasks').document(str(tid_expected)).delete()
+    delete_user(user_id2)
+    reset_projects()
+    reset_project_count()
+    """db.collection('tasks').document(str(tid_expected)).delete()
     db.collection('achievements').document('night_owl').delete()
     db.collection('reviews').document(str(rid_expected)).delete()"""
 
@@ -48,6 +61,37 @@ def test_welcome_notification():
     assert actual_notification.get('has_read') == False
     assert actual_notification.get('notification_msg') == "Welcome to TaskForge, John Doe. You can view future notifications here!"
     assert isinstance(actual_notification.get('time_sent'), datetime)
+    
+def test_project_invite_notification():
+    '''
+    Test to ensure invite_to_project() adds invite notifications in the database
+    '''
+    receiver_uids = [user_id1, user_id2]
+    invite_to_project(pid_expected, user_id0, receiver_uids)
+
+    # Assert user 1 has invite notification data #
+    doc_data = db.collection('notifications').document(user_id1).get().to_dict()
+    actual_notification = doc_data.get('project_invite0')
+
+    assert actual_notification.get('has_read') == False
+    assert actual_notification.get('notification_msg') == "John Doe has invited you to join Project N."
+    assert actual_notification.get('pid') == pid_expected
+    assert isinstance(actual_notification.get('time_sent'), datetime)
+    assert actual_notification.get('type') == 'project_invite'
+    assert actual_notification.get('uid_sender') == user_id0
+    assert actual_notification.get('accept_msg') == "You accepted John Doe's project invitation."
+    assert actual_notification.get('decline_msg') == "You declined John Doe's project invitation."
+    assert actual_notification.get('nid') == 'project_invite0'
+
+    # Assert user 2 has invite notification data; skip some data #
+    doc_data = db.collection('notifications').document(user_id2).get().to_dict()
+    actual_notification = doc_data.get('project_invite0')
+
+    assert actual_notification.get('notification_msg') == "John Doe has invited you to join Project N."
+    assert actual_notification.get('pid') == pid_expected
+    assert actual_notification.get('uid_sender') == user_id0
+    assert actual_notification.get('nid') == 'project_invite0'
+
     remove_test_data()
 
 # COMMENTED OUT TESTS THAT ARE WAITING FOR IT'S PARENT FUNCTION TO BE IMPLEMENTED
@@ -65,21 +109,6 @@ def test_welcome_notification():
     assert actual_notification.get('uid_sender') == 'NotifyUser1'
     assert actual_notification.get('accept_msg') == "You accepted Jane Doe's connection request."
     assert actual_notification.get('decline_msg') == "You declined Jane Doe's connection request."
-
-def test_project_invite_notification():
-    notification_project_invite(user_id0, user_id1, pid_expected)
-
-    doc_data = db.collection('notifications').document(user_id0).get().to_dict()
-    actual_notification = doc_data.get('project_invite0')
-
-    assert actual_notification.get('has_read') == False
-    assert actual_notification.get('notification_msg') == "Jane Doe has invited you to join Project Notification !!! NOTIFICATION TEST."
-    assert actual_notification.get('pid') == pid_expected
-    assert isinstance(actual_notification.get('time_sent'), datetime)
-    assert actual_notification.get('type') == 'project_invite'
-    assert actual_notification.get('uid_sender') == 'NotifyUser1'
-    assert actual_notification.get('accept_msg') == "You accepted Jane Doe's project invitation."
-    assert actual_notification.get('decline_msg') == "You declined Jane Doe's project invitation."
 
 def test_assigned_task_notification():
     notification_assigned_task(user_id0, pid_expected, tid_expected)
