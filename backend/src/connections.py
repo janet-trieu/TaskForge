8a5ee314797644889dc0177ee8b7d517671427cb
Vincent <z5207667@ad.unsw.edu.au>
2023-03-18 12:48:10 +1100

    added stubs

--- backend/src/connections.py
+++ backend/src/connections.py
@@ -1,1 +1,15 @@
+from global_counters import *
+import sys
+from error import InputError, AccessError
 
+def connection_request_respond():
+    pass
+    
+def get_connection_requests():
+    pass
+
+def send_connection_request():
+    pass
+
+def get_connected_taskmasters():
+    pass
