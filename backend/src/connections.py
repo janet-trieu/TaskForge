1a48a32a9d00a62227d73761aa653dbbf858af90
Vincent <z5207667@ad.unsw.edu.au>
2023-03-18 18:33:38 +1100

    implementation + some tests

--- backend/src/connections.py
+++ backend/src/connections.py
@@ -3,16 +3,33 @@ import sys
 from error import InputError, AccessError
 from helper import *
 from notifications import does_nid_exists
+import firestore
 
 def connection_request_respond(uid, nid, response):
     check_valid_uid(uid)
     if (not does_nid_exists(nid)): raise InputError('Notification invalid')
     if (not isinstance(response, bool)): raise InputError('Response should be bool')
     
+    nid_ref = db.collections('notifications').document(nid)
+    uid_sender = nid_ref.get().get('uid_sender')
+    db.collections('notifications').document(nid).delete()
+    if (not response): return
+    u_ref = db.collections('users').document(uid)
+    u_ref.update({'connections' : firestore.ArrayUnion([uid_sender])})
+    u_ref = db.collections('users').document(uid_sender)
+    u_ref.update({'connections' : firestore.ArrayUnion([uid])})
     
-    
-def get_connection_requests():
-    pass
+def get_connection_requests(uid):
+    check_valid_uid(uid)
+    conn_list = []
+    notis = db.collections('notifications').where('uid', '==', uid).stream()
+    for noti in notis:
+        result = noti.to_dict()
+        if (result.get('notification_type') == 'connection_request'):
+            conn_list.append(result)
+    return conn_list
 
-def get_connected_taskmasters():
-    pass
+def get_connected_taskmasters(uid):
+    check_valid_uid(uid)
+    user_ref = db.collections('users').document(uid)
+    return user_ref.get().get('connections')
--- backend/tests/connections_test.py
+++ backend/tests/connections_test.py
@@ -1,1 +1,31 @@
+import pytest
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+from src.connections import connection_request_respond, get_connection_requests, get_connected_taskmasters
+from src.error import *
+from src.helper import *
+
+
+def test_uid_type_connection_request_respond():
+    try:
+        connection_request_respond(1, 2, True)
+    except InputError:
+        pass
+
+def test_success_connection_request_respond():
+    pass
+
+
+def test_uid_type_get_connection_requests():
+    try:
+        get_connection_requests(1)
+    except InputError:
+        pass
+        
+def test_uid_type_get_connected_taskmasters():
+    try:
+        get_connected_taskmasters(1)
+    except InputError:
+        pass
