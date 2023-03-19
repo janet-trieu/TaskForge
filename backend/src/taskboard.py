98ea6ea7c50e5a48d7a54868ced23f887481e789
unknown <Francis Nguyen>
2023-03-19 20:18:33 +1100

    created skeleton functions for taskboard

--- backend/src/taskboard.py
+++ backend/src/taskboard.py
@@ -6,3 +6,30 @@ from .global_counters import *
 from .classes import User
 from .error import *
 from .notifications import *
+
+### ========= Create Task ========= ###
+def create_task(uid):
+    """
+    Creates a task and initialises the task into firestore database
+
+    Args:
+        uid (str): uid of the user that can be found in auth database
+
+    Returns:
+        An int that corresponds to the id to the task.
+    """
+    return
+
+### ========= Delete Task ========= ###
+def delete_task(tid):
+    """
+    Deletes a task from firestore database
+
+    Args:
+        tid (int): id of the task that can be found in firestore database
+
+    Returns:
+        None
+    """
+    return
+
