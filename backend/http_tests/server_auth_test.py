193bcd1398bdfae4291d678502662a64edb553e7
unknown <janet.trieu@outlook.com>
2023-03-19 23:45:52 +1100

    Removed direct uid string calls and replaced with uids made during tests

--- backend/http_tests/server_auth_test.py
+++ backend/http_tests/server_auth_test.py
@@ -2,21 +2,37 @@
 Test file for Flask http testing of authentication reset password (user invoked) functionality
 '''
 import requests
+from src.authentication import *
+from src.test_helpers import *
+
 port = 5000
 url = f"http://localhost:{port}/"
+
+try:
+    create_user_email("authtest0@gmail.com", "password123", "Auth Doe")
+except auth.EmailAlreadyExistsError:
+    pass
+
+user_id = auth.get_user_by_email("authtest0@gmail.com").uid
 
 def test_reset_password():
 
     reset_resp = requests.post(url + "authentication/reset_password", headers={
-        "Authorization": "3TssNFyMXmOtTGarpRtAZ7VrOd72"
+        "Authorization": user_id
     })
 
     assert reset_resp.status_code == 200
 
 def test_reset_password_invalid_uid():
 
+    invalid_user_id = "my name is invalid uid and i hate being valid"
+
     reset_resp = requests.post(url + "authentication/reset_password", headers={
-        "Authorization": "Xaao51rHZ9fJK6U6J0NtPixAh0k2"
+        "Authorization": invalid_user_id
     })
 
     assert reset_resp.status_code == 400
+
+# Reset database
+delete_user(user_id)
+reset_firestore_database()
--- backend/http_tests/server_profile_test.py
+++ backend/http_tests/server_profile_test.py
@@ -4,14 +4,23 @@ from subprocess import Popen, PIPE
 import signal
 from time import sleep
 import requests
+from src.authentication import *
+from src.test_helpers import *
  
 url = 'http://127.0.0.1:5000'
+
+try:
+    create_user_email("profiletest0@gmail.com", "password123", "Profile Doe")
+except auth.EmailAlreadyExistsError:
+    pass
+
+user_id = auth.get_user_by_email("profiletest0@gmail.com").uid
 
 def test_user_details_success():
     """
     Successfully accessing a user's details
     """
-    json_dict = {'Authorization': 'sklzNex5udNeOd65uvsuGAYBNkH2'}
+    json_dict = {'Authorization': user_id}
     resp = requests.get(url + '/profile/details', headers=json_dict)
 
     assert resp.status_code == 200
@@ -20,7 +29,7 @@ def test_user_details_failure():
     """
     Failing to access a user's details as they do not exist
     """
-    json_dict = {'Authorization': 'sklzNex5udNeOd6dsf5uvsuGAYBNkH2'}
+    json_dict = {'Authorization': user_id}
     resp = requests.get(url + '/profile/details', headers=json_dict)
 
     assert resp.status_code == 400
@@ -30,7 +39,7 @@ def test_update_profile_success():
     """
     Succeeding to update a user's details
     """
-    headers_dict = {'Authorization': 'sklzNex5udNeOd65uvsuGAYBNkH2'}
+    headers_dict = {'Authorization': user_id}
     json_dict = {'email': 'ilovedabin@gmail.com', 'role': "duck engineer", 'photo_url': "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSoPravXdZRihoO83Kd7TwlZBik03ZXlDZvBYx5ZyotO_RWKE7d_G_nFxBTjPE1yTILP7qUl2Q_rtbLUsk"}
     resp = requests.put(url + '/profile/update', headers=headers_dict, json=json_dict)
 
@@ -41,7 +50,7 @@ def test_update_profile_failure():
     Failing to  update a user's details with an invalid email
     However, should still update rest of the details
     """
-    headers_dict = {'Authorization': 'sklzNex5udNeOd65uvsuGAYBNkH2'}
+    headers_dict = {'Authorization': user_id}
     json_dict = {'email': 'ilovedabin', 'role': "balls engineer", 'photo_url': ""}
     resp = requests.put(url + '/profile/update', headers=headers_dict, json=json_dict)
 
@@ -51,7 +60,7 @@ def test_get_tasks_success():
     """
     Succeeding in getting a user's tasks
     """
-    json_dict = {'Authorization': 'sklzNex5udNeOd65uvsuGAYBNkH2'}
+    json_dict = {'Authorization': user_id}
     resp = requests.get(url + '/profile/tasks', headers=json_dict)
 
     assert resp.status_code == 200
@@ -60,7 +69,11 @@ def test_get_tasks_success():
     """
     Failing in getting a user's tasks
     """
-    json_dict = {'Authorization': 'sklzNex5udNeOd65usvsuGAYBNkH2'}
+    json_dict = {'Authorization': user_id}
     resp = requests.get(url + '/profile/tasks', headers=json_dict)
 
     assert resp.status_code == 400
+
+# Reset database
+delete_user(user_id)
+reset_firestore_database()
