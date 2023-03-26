de60400977435bd894f87761939ce1017d721ecb
Vincent <z5207667@ad.unsw.edu.au>
2023-03-27 00:57:33 +1100

    further clean up

--- backend/storage_examples.py
+++ backend/storage_examples.py
@@ -1,10 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, initialize_app, storage
 from google.oauth2 import service_account
-# Init firebase with your credentials
+
+
+
 cred = credentials.Certificate("taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json")
 initialize_app(cred, {'storageBucket': 'taskforge-9aea9.appspot.com'})
-credentials = service_account.Credentials.from_service_account_file("taskforge-9aea9-firebase-adminsdk-xaffr-c80ed6513a.json")
 
 def upload_file(fileName, destination_name):
     bucket = storage.bucket()
@@ -13,14 +14,10 @@ def upload_file(fileName, destination_name):
     blob.make_public()
     print(f"{destination_name}", blob.public_url)
 
-
-base_url = 'https://storage.googleapis.com/taskforge-9aea9.appspot.com/'
-
 def download_file(fileName, destination_name): 
     bucket = storage.bucket()
     blob = bucket.blob(fileName)
     blob.download_to_filename(destination_name)
-    
 
 def delete_file(fileName):
     bucket = storage.bucket()
