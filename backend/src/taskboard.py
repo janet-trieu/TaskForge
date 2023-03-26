fc9d957c2bf8fb44fb664b01f95b7843b50a9384
unknown <Francis Nguyen>
2023-03-26 13:24:46 +1100

    bug fixes

--- backend/src/taskboard.py
+++ backend/src/taskboard.py
@@ -31,11 +31,12 @@ def create_epic(uid, pid, title, description, colour):
     epic_ref = db.collection("epics")
     value = get_curr_eid()
     epic = Epic(value, pid, [], title, description, colour)
-    epic_ref.document(value).set(epic.to_dict())
+    epic_ref.document(str(value)).set(epic.to_dict())
+    update_eid()
     return value
 
 ### ========= Get Epic Ref ========= ###
-def get_epic_ref(uid, eid):
+def get_epic_ref(eid):
     """
     Gets an Epic reference from firestore database
 
@@ -45,7 +46,9 @@ def get_epic_ref(uid, eid):
     Returns:
         An Epic document from firestore that corresponds to the EID given. 
     """
+    # Check if user is in project
     check_valid_eid(eid)
+
     return db.collection('epics').document(str(eid)).get()
 
 ### ========= Get Epic Details ========= ###
@@ -59,6 +62,10 @@ def get_epic_details(uid, eid):
     Returns:
         A dict with the full details of the epic
     """
+    # Check if user is in project
+    check_valid_eid(eid)
+    check_user_in_project(uid, get_epic_ref(eid).get("pid"))
+
     doc = get_epic_ref(eid)
     epic = Epic(doc.get("eid"), doc.get("pid"), doc.get("tasks"), doc.get("title"), doc.get("description"), doc.get("colour"))
     return epic.to_dict
@@ -77,7 +84,7 @@ def delete_epic(uid, eid):
     """
     # Check if user is in project
     check_valid_eid(eid)
-    check_user_in_project(uid, get_epic_ref(uid, eid).get("pid"))
+    check_user_in_project(uid, get_epic_ref(eid).get("pid"))
     tasks = get_epic_ref(eid).get("tasks")
 
     # Remove eid in every child task and subtask
@@ -117,13 +124,20 @@ def create_task(uid, pid, eid, assignees, title, description, deadline, workload
 
     task_ref = db.collection("tasks")
     value = get_curr_tid()
-    task = Task(value, pid, eid, assignees, [], title, description, deadline, workload, priority, status, [], False, "")
-    task_ref.document(value).set(task.to_dict())
+    task = Task(value, pid, eid, "", [], title, description, deadline, workload, priority, status, [], False, "")
+    task_ref.document(str(value)).set(task.to_dict())
 
-    assign_task(value, assignees)
+    #Assign task to assignees
+    assign_task(uid, value, assignees)
+    # Add task to epic
+    epic_tasks = db.collection('epics').document(str(eid)).get().get("tasks")
+    epic_tasks.append(value)
+    db.collection('epics').document(str(eid)).update({"tasks": epic_tasks})
+    # update tid
+    update_tid()
     return value
 ### ========= Get Task Ref ========= ###
-def get_task_ref(uid, tid):
+def get_task_ref(tid):
     """
     Gets a task reference from firestore database
 
@@ -135,7 +149,6 @@ def get_task_ref(uid, tid):
     """
     # Check if user is in project
     check_valid_tid(tid)
-    check_user_in_project(uid, get_task_ref(uid, tid).get("pid")) 
     return db.collection('tasks').document(str(tid)).get()
 
 
@@ -152,7 +165,7 @@ def get_task_details(uid, tid):
     """
     # Check if user is in project
     check_valid_tid(tid)
-    check_user_in_project(uid, get_task_ref(uid, tid).get("pid"))
+    check_user_in_project(uid, get_task_ref(tid).get("pid"))
 
     doc = get_task_ref(tid)
     task = Task(doc.get("tid"), doc.get("pid"), doc.get("eid"), doc.get("assignees"), doc.get("subtasks"), doc.get("title"), 
@@ -166,6 +179,7 @@ def assign_task(uid, tid, new_assignees):
     Assigns new users to a task
 
     Args:
+        uid (str): id of the user thats assigning tasks
         tid (int): id of the task that can be found in firestore database
         new_assignees (list): a list of UIDs (str) that will become the new assignee list
     
@@ -174,7 +188,7 @@ def assign_task(uid, tid, new_assignees):
     """
     # Check if user is in project
     check_valid_tid(tid)
-    check_user_in_project(uid, get_task_ref(uid, tid).get("pid"))
+    check_user_in_project(uid, get_task_ref(tid).get("pid"))
     # Check if all UIDs in new_assignee are valid and in the project
     pid = get_task_ref(tid).get("pid")
     old_assignees = get_task_ref(tid).get("assignees")
@@ -189,13 +203,18 @@ def assign_task(uid, tid, new_assignees):
     for uid in removed_assignees:
         user = get_user_ref(uid)
         tasks = user.get("tasks")
-        user.update({"tasks": tasks.remove(tid)})
+        db.collection('users').document(new_uid).update({"tasks": tasks.remove(tid)})
 
     # add task to new assignees
-    for uid in added_assignees:
-        user = get_user_ref(uid)
+    for new_uid in added_assignees:
+        user = get_user_ref(new_uid)
         tasks = user.get("tasks")
-        user.update({"tasks": tasks.append(tid)})
+        if (tasks is None):
+            db.collection('users').document(new_uid).update({"tasks": [tid]})
+        else:
+            tasks.append(tid)
+            db.collection('users').document(new_uid).update({"tasks": tasks})
+    db.collection('tasks').document(str(tid)).update({"assignees": new_assignees})
     return
 
 ### ========= Delete Task ========= ###
@@ -224,6 +243,13 @@ def delete_task(uid, tid):
     tasks = epic.get("tasks")
     tasks.remove("tid")
     epic.update({"tasks": tasks})
+
+    # Remove task from assigned users
+    assignees = db.collection('tasks').document(str(tid)).get("assignees")
+    for assignee in assignees:
+        assigned_tasks = db.collection('users').document(str(assignee)).get("tasks")
+        assigned_tasks.remove(tid)
+        db.collection('users').document(str(assignee)).update({"tasks": assigned_tasks})
 
     db.collection('tasks').document(str(tid)).delete()
     return 
@@ -254,14 +280,15 @@ def create_subtask(uid, tid, pid, eid, assignees, title, description, deadline, 
 
     subtask_ref = db.collection("subtasks")
     value = get_curr_stid()
-    subtask = Subtask(value, tid, pid, eid, assignees, title, description, deadline, workload, priority, status)
+    subtask = Subtask(value, tid, pid, eid, "", title, description, deadline, workload, priority, status)
     subtask_ref.document(value).set(subtask.to_dict())
 
-    assign_subtask(value, assignees)
+    assign_subtask(uid, value, assignees)
+    update_stid()
     return value
 
 ### ========= Get Subtask Ref ========= ###
-def get_subtask_ref(uid, stid):
+def get_subtask_ref(stid):
     """
     Gets a subtask reference from firestore database
 
@@ -273,7 +300,7 @@ def get_subtask_ref(uid, stid):
     """
     # Check if user is in project
     check_valid_stid(stid)
-    check_user_in_project(uid, get_subtask_ref(uid, stid).get("pid"))    
+ 
     return db.collection('subtasks').document(stid).get()
 
 ### ========= Get Subtask Details ========= ###
@@ -288,11 +315,12 @@ def get_subtask_details(uid, stid):
         A dict with the full details of the task
     """
     # Check if user is in project
-    check_user_in_project(uid, get_subtask_ref(uid, stid).get("pid"))
+    check_user_in_project(uid, get_subtask_ref(stid).get("pid"))
     check_valid_stid(stid)
     doc = get_subtask_ref(stid)
     subtask = Subtask(doc.get("stid"), doc.get("tid"), doc.get("pid"), doc.get("eid"),doc.get("assignees"), doc.get("title"), 
-                doc.get("description"), doc.get("deadline"), doc.get("workload"), doc.get("priority"), doc.get("status"))
+                doc.get("description"), doc.get("deadline"), doc.get("workload"), doc.get("priority"), doc.get("status"),
+                doc.get("comments"), doc.get("flagged"), doc.get("completed"))
     return subtask.to_dict
 
 ### ========= Get Assign Subtask ========= ###
@@ -326,10 +354,15 @@ def assign_subtask(uid, stid, new_assignees):
         user.update({"subtasks": subtasks.remove(stid)})
 
     # add subtask to new assignees
-    for uid in added_assignees:
-        user = get_user_ref(uid)
+    for new_uid in added_assignees:
+        user = get_user_ref(new_uid)
         subtasks = user.get("subtasks")
-        user.update({"subtasks": subtasks.append(stid)})
+        if (subtasks is None):
+            db.collection('users').document(new_uid).update({"subtasks": [stid]})
+        else:
+            subtasks.append(tid)
+            db.collection('users').document(new_uid).update({"subtasks": subtasks})
+    db.collection('tasks').document(str(stid)).update({"assignees": new_assignees})
     return
 
 ### ========= Delete Subtask ========= ###
@@ -343,6 +376,53 @@ def delete_subtask(uid, stid):
     Returns:
         None
     """
-    check_user_in_project(uid, get_subtask_ref(uid, stid).get("pid"))
+    check_user_in_project(uid, get_subtask_ref(stid).get("pid"))
     check_valid_stid(stid)
+    # Remove task from assigned users
+    assignees = db.collection('tasks').document(str(stid)).get("assignees")
+    for assignee in assignees:
+        assigned_subtasks = db.collection('users').document(str(assignee)).get("subtasks")
+        assigned_subtasks.remove(stid)
+        db.collection('users').document(str(assignee)).update({"tasks": assigned_subtasks})
     return db.collection('subtasks').document(str(stid)).delete()
+
+### ========= Search Taskboard ========= ###
+def search_taskboard(uid, pid, query):
+    """
+    Searches the project and returns a list of tasks that match the query
+
+    Args:
+        uid (str): id of the user requesting the search
+        pid (int): id of the project that is being searched
+        query (str): string of the query that will be compared to tasks
+
+    Returns:
+        A list of tasks that match the query
+    """
+    return
+
+### ========= Comment Task ========= ###
+def comment_task(uid, tid, comment):
+    """
+    Comments on a task
+
+    Args:
+        uid (str): id of the user commenting
+        tid (int): id of the task that will be commented on
+        comment (str): comment that will be commented
+
+    Returns:
+        None
+    """
+    return
+
+### ========= Flag Task ========= ###
+def flag_task(uid, tid, boolean):
+    """
+    Sets task flagness as boolean
+
+    Args:
+        uid (str): id of the user
+    """
+    
+    return
