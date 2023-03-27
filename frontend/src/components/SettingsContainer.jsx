0a2f0df21f5dc343c49175d9f2c41c38464a1a82
unknown <janet.trieu@outlook.com>
2023-03-27 19:33:43 +1100

    Made SettingsContainer to be more reusable for future containers

--- frontend/src/components/SettingsContainer.jsx
+++ frontend/src/components/SettingsContainer.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import './SettingsContainer.css'
-import banIcon from '../assets/settings ban user.png';
-import adminIcon from '../assets/settings admin user.png';
 
-const SettingsContainer = () => {
+const SettingsContainer = ({ title, description, buttons }) => {
   return (
     <>
       <div className="settings-container">
-        <h3>Admin</h3>
-        <p style={{ color: "gray" }}>Manage and configure user accounts.</p>
-        <div className="divider"></div>
-        <div className="buttons">
-          <button><img src={adminIcon} />Admin User</button>
-          <button><img src={banIcon} />Ban User</button>
+        <h3 className="settings-title">{title}</h3>
+        <p className="settings-description">{description}</p>
+        <div className="settings-divider"></div>
+        <div className="settings-buttons">
+          {buttons.map(button =>
+            <button key={button.id}>
+              <img src={button.icon}></img>
+              {button.label}
+            </button>)}
         </div>
       </div>
     </>
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import SettingsContainer from '../components/SettingsContainer.jsx';
 
+import banIcon from '../assets/settings ban user.png';
+import adminIcon from '../assets/settings admin user.png';
+
+const adminButtons = [
+  { id: 1, icon: adminIcon, label: 'Admin User' },
+  { id: 2, icon: banIcon, label: 'Ban User' },
+]
+
 const Settings = () => {
   return (
     <>
-      <SettingsContainer />
+      <SettingsContainer title="Admin" description="Manage and configure user accounts." buttons={adminButtons} />
     </>
   )
 }
