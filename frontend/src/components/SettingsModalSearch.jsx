a60ef0f9a51d378a9cc807c46b7c42fc4b2aa1e5
unknown <janet.trieu@outlook.com>
2023-03-28 01:26:49 +1100

    Replaced confirm modal with window confirm

--- frontend/src/components/SettingsModalSearch.jsx
+++ frontend/src/components/SettingsModalSearch.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import './SettingsModal.css'
 import './Searchbar.css';
-import { Modal } from "@mui/material";
-import SettingsModalConfirm from "./SettingsModalConfirm";
 
 const SettingsModalSearch = ({ title, onClose, action, warning }) => {
-    const [open, setOpen] = useState(false);
-    const handleClose = () => { setOpen(false) };
-    const handleOpen  = () => { setOpen(true) };
+    const handleConfirm = (action, warning) => {
+        const msg = `Are you sure you want to ${action.toLowerCase()} FULLNAME UID? ${warning}`
+    
+        if (window.confirm(msg)) {
+            // if action == admin, admin user
+            // if action == ban, remove user
+            onClose()
+        } else {
+            onClose()
+        }
+    }
 
     return (
         <>
@@ -21,13 +27,10 @@ const SettingsModalSearch = ({ title, onClose, action, warning }) => {
                     </div>
                     <div className="settings-modal-footer">
                         <button className="button-cancel" onClick={onClose}>Cancel</button>
-                        <button className="button-confirm" onClick={handleOpen}>{action}</button>
+                        <button className="button-confirm" onClick={() => handleConfirm(action, warning)}>{action}</button>
                     </div>
                 </div>
             </div>
-            <Modal open={open} onClose={handleClose}>
-                <SettingsModalConfirm title={title} onClose={handleClose} action={action} warning={warning} />
-            </Modal>
         </>
     )
 }
