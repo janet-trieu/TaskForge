8d0c459cd79ec30b1ee7ff12d1f8623ffc81c402
unknown <janet.trieu@outlook.com>
2023-03-30 01:59:17 +1100

    Added 'Account' container with 'Reset Password' button and overlay

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import SettingsContainer from '../components/SettingsContainer.jsx';
-import SettingsModalConfirm from "../components/SettingsModalConfirm.jsx";
 
 import banIcon from '../assets/settings ban user.png';
 import adminIcon from '../assets/settings admin user.png';
+import passwordIcon from '../assets/settings reset password.png'
 
 const adminButtons = [
-  { id: 1, icon: adminIcon, action: 'Admin', warning: 'This will give the user admin permissions.' },
-  { id: 2, icon: banIcon, action: 'Ban', warning: 'This will remove all existing details permanently.'},
+  { id: 1, icon: adminIcon, action: 'Admin User', warning: 'This will give the user admin permissions.', type: 'uid search' },
+  { id: 2, icon: banIcon, action: 'Ban User', warning: 'This will remove all existing details permanently.', type: 'uid search'},
+]
+
+const accountButtons = [
+  { id: 1, icon: passwordIcon, action:'Reset Password', warning: 'This will send a reset password link to your email.', type: 'confirm'}
 ]
 
 const Settings = () => {
   return (
     <>
       <SettingsContainer title="Admin" description="Manage and configure user accounts." buttons={adminButtons} />
+      <SettingsContainer title="Account" description="Manage and configure your account." buttons={accountButtons} />
     </>
   )
 }
