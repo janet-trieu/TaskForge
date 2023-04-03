699495466533ed967355529aadefe87c88abb09e
Dabin Haam <dabin.haam@gmail.com>
2023-04-03 20:23:49 +1000

    continuing work on achievements

--- backend/src/profile_page.py
+++ backend/src/profile_page.py
@@ -3,7 +3,7 @@ from firebase_admin import firestore
 from firebase_admin import auth
 
 from .global_counters import *
-from .classes import User
+from .classes import *
 from .error import *
 from .notifications import *
 
