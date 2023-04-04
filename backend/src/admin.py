4e7ce2cc4a5d8036e7a20570e081eeba0de1ff74
Vincent <z5207667@ad.unsw.edu.au>
2023-04-05 01:59:56 +1000

    removed readd_user, remove user erases user in db

--- backend/src/admin.py
+++ backend/src/admin.py
@@ -75,23 +75,26 @@ def remove_user(uid_admin, uid_user):
     if (not is_admin(uid_admin)): raise InputError('You are not an admin')
     if (is_removed(uid_user)): raise InputError('User is already removed')
     
-    user_ref = db.collection("users").document(uid_user)
-    user_ref.update({'is_removed': True})
-    return {}
-
-'''
-Undoes a removal
-Args:
-    - uid_admin(string): uid of the admin readding user
-    - uid_user(string): uid of user who will be readded
-'''
-def readd_user(uid_admin, uid_user):
-    if (not isinstance(uid_admin, str) or not isinstance(uid_user, str)): raise InputError('uids are strings')
-    if (not get_user_ref(uid_admin) or not get_user_ref(uid_user)): raise InputError('uid invalid')
-    if (is_banned(uid_admin)): raise AccessError('You are banned')
-    if (not is_admin(uid_admin)): raise InputError('You are not an admin')
-    if (not is_removed(uid_user)): raise InputError('User is not removed')
+    #delete user field
+    user_ref = db.collection('users').document(uid_user)
+    user_ref.update({uid_user: firestore.DELETE_FIELD})
     
-    user_ref = db.collection("users").document(uid_user)
-    user_ref.update({'is_removed': False})
+    #delete notifications of users
+    user_ref = db.collection('notifications').document(uid_user)
+    user_ref.update({uid_user: firestore.DELETE_FIELD})
+    
+    #remove from projects
+    projs = db.collection('users').document(uid_user).get().get("projects")
+    for proj in projs:
+        proj_mems = db.collection('projects').document(proj).get().get('project_members')
+        proj_mems.remove(uid_user)
+        db.collection("project").document(uid_user).update({"project_members": proj_mems})
+        
+    #remove from tasks
+    tasks = db.collection('users').document(uid_user).get().get("tasks")
+    for task in tasks:
+        assignees = db.collection('tasks').document(task).get().get('assignees')
+        assignees.remove(uid_user)
+        db.collection("tasks").document(uid_user).update({"assignees": assignees})
+        
     return {}
