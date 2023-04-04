a8703e1993865240b38b32835b5749cb87a5b858
unknown <janet.trieu@outlook.com>
2023-04-04 18:29:03 +1000

    Fixed connection card data + attempt to add default image for users

--- frontend/src/components/ConnectionCard.jsx
+++ frontend/src/components/ConnectionCard.jsx
@@ -3,31 +3,16 @@ import { makeRequest } from "../helpers";
 import './ConnectionCard.css';
 import userIcon from '../assets/default user icon.png';
 
-const ConnectionCard = ({ firebaseApp, uid }) => {
-  const [isLoading, setIsLoading] = useState('Loading...');
-  const [connectionDetails, setConnectionDetails] = useState(null);
-
-  useEffect(async () => {
-
-    const data = await makeRequest('/connections/details', 'GET', {uid: uid}, firebaseApp.auth().currentUser.uid);
-    if (data.error) alert(data.error);
-    else {
-      setConnectionDetails(data);
-      setIsLoading(false);
-    }
-  }, []);
-
+const ConnectionCard = ({ firebaseApp, photo_url, displayName, role }) => {
   return (
-    isLoading || (
       <div className="connection-card">
-        <img src={userIcon}></img>
+        <img src={photo_url}></img>
         <div className="connection-card-info">
-          <div style={{fontWeight: 'bold'}}>{connectionDetails.display_name}</div>
-          <div style={{color: 'gray'}}>{connectionDetails.role}</div>
+          <div style={{fontWeight: 'bold'}}>{displayName}</div>
+          <div style={{color: 'gray'}}>{role}</div>
         </div>
       </div>
     )
-  )
 }
 
 export default ConnectionCard;
