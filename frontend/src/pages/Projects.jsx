0657d558b9f3ddc1f9cce51cd94f8bc6ededac23
unknown <janet.trieu@outlook.com>
2023-04-05 07:16:11 +1000

    Implemented pin/unpin frontend

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -14,6 +14,7 @@ const Projects = ({ firebaseApp }) => {
     else {
       setProjects(data);
       setIsLoading(false);
+      console.log(data)
     }
   }, [showCompleted])
 
@@ -24,7 +25,8 @@ const Projects = ({ firebaseApp }) => {
         {isLoading || (
           projects.map((details, idx) => {
             if (showCompleted || details.status !== "Completed") {
-              return <ProjectCard name={details.name} description={details.description} status={details.status} picture={details.picture} pid={details.pid} key={idx} />
+              console.log(details.is_pinned)
+              return <ProjectCard name={details.name} description={details.description} status={details.status} picture={details.picture} pid={details.pid} key={idx} isPinned={details.is_pinned} uid={firebaseApp.auth().currentUser.uid}/>
             }
           })
         )}
