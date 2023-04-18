00489b97e8c169f8b9fff36e5f4c4279345ba782
Joseph Saad <joseph.saad137@gmail.com>
2023-04-18 19:05:51 +1000

    subtasks

--- frontend/src/components/TaskSubtasksModalContent.jsx
+++ frontend/src/components/TaskSubtasksModalContent.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, useState } from "react";
 import { makeRequest } from "../helpers";
 import { Modal } from "@mui/material";
+import SubtaskCard from "./SubtaskCard";
 import TaskSubtasksCreateModalContent from "./TaskSubtasksCreateModalContent";
 
 const TaskSubtasksModalContent = forwardRef((props, ref) => {
@@ -10,14 +11,30 @@ const TaskSubtasksModalContent = forwardRef((props, ref) => {
   const handleOpenCreate = () => { setOpenCreate(true) };
   const handleCloseCreate = () => { setOpenCreate(false) };
 
+  const testSubtask = {
+    stid: "EXA-001",
+    title: "This is the task title.",
+    description: "This is the description of the subtask.",
+    status: "In Progress",
+    deadline: "01/01/2001",
+    workload: 3,
+    priority: "Low",
+    assignees: ["test@email.com", "test1@email.com", "test2@email.com"]
+  }
+
   return (
     <div id="task-subtasks-modal">
       {/* Create subtask option */}
-      <button type="button" onClick={handleOpenCreate}>Create Subtask</button>
+      <button type="button" style={{width: '100%', marginBottom: '2em'}} onClick={handleOpenCreate}>Create Subtask</button>
       <Modal open={openCreate} onClose={handleCloseCreate}>
         <TaskSubtasksCreateModalContent uid={props.uid} tid={props.tid} handleClose={handleCloseCreate} />
       </Modal>
       {/* View subtasks */}
+      <div id="subtask-card-container">
+        <SubtaskCard uid={props.uid} subtask={testSubtask}/>
+        <SubtaskCard uid={props.uid} subtask={testSubtask}/>
+        <SubtaskCard uid={props.uid} subtask={testSubtask}/>
+      </div>
     </div>
   );
 });
