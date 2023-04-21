22170a3ea5199a27637171bd0fbebbb6f1030a24
Dabin Haam <dabin.haam@gmail.com>
2023-04-21 20:52:44 +1000

    fixes

--- backend/http_tests/server_admin_test.py
+++ backend/http_tests/server_admin_test.py
@@ -6,14 +6,16 @@ import pytest
 import re
 from subprocess import Popen, PIPE
 import signal
+from time import sleep
 import requests
 
 from src.profile_page import *
 from src.helper import *
+from src.test_helpers import reset_database
 
 # test set up
 port = 8000
-url = f'http://127.0.0.1:{port}'
+url = f'http://127.0.0.1/%7Bport%7D'
 
 try:
     admin_uid = create_user_email("admin@gmail.com", "admin123", "Admin Admin")
@@ -32,7 +34,7 @@ def test_give_admin_success():
     Successfully giving admin to another user
     """
     headers_dict = {'Authorization': admin_uid}
-    json_dict = {'uid_admin': get_email(user_uid)}
+    json_dict = {'uid_user': get_email(user_uid)}
     resp = requests.post(url + '/admin/give_admin', headers=headers_dict, json=json_dict)
 
     assert resp.status_code == 200
@@ -42,17 +44,17 @@ def test_ban_user_success():
     Successfully banning user by an admin
     """
     headers_dict = {'Authorization': admin_uid}
-    json_dict = {'uid_admin': get_email(user_uid)}
+    json_dict = {'uid_user': get_email(user_uid)}
     resp = requests.post(url + '/admin/ban_user', headers=headers_dict, json=json_dict)
 
     assert resp.status_code == 200
-    
+
 def test_unban_user_success():
     """
     Successfully unbanning user by an admin
     """
     headers_dict = {'Authorization': admin_uid}
-    json_dict = {'uid_admin': get_email(user_uid)}
+    json_dict = {'uid_user': get_email(user_uid)}
     resp = requests.post(url + '/admin/unban_user', headers=headers_dict, json=json_dict)
 
     assert resp.status_code == 200
@@ -62,7 +64,7 @@ def test_remove_user_success():
     Successfully removing user by an admin
     """
     headers_dict = {'Authorization': admin_uid}
-    json_dict = {'uid_admin': get_email(user_uid)}
+    json_dict = {'uid_user': get_email(user_uid)}
     resp = requests.post(url + '/admin/remove_user', headers=headers_dict, json=json_dict)
 
     assert resp.status_code == 200
